e530ebc19a0f5fb7bc535a332fc95617d7c0e169
bazarniy <kartofka@gmail.com>
2016-11-24 13:29:32 +0300

    PathValidator TDD experiment

--- UniversalParser/Base/Utilities/PathValidator.cs
+++ UniversalParser/Base/Utilities/PathValidator.cs
@@ -1,48 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Base.Utilities
+﻿namespace Base.Utilities
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
     using System.Text.RegularExpressions;
 
     public static class PathValidator
     {
         public static void ValidateFilePath(string path)
         {
-            if (path == null) throw new ArgumentNullException(nameof(path));
-            if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("path is empty", nameof(path));
-            if (IsShare(path)) throw new ArgumentException("is share path", nameof(path));
+            BasePathValidation(path);
 
-            var label = GetPathRoot(path);
-            if (!ValidateDiskLabel(label)) throw new ArgumentException($"wrong disk label '{label}'");
+            var filename = GetLastPathSegment(path);
+            if (string.IsNullOrWhiteSpace(filename)) throw new ArgumentException("filename is empty");
+            if (!IsValidFileNameChars(filename)) throw new ArgumentException($"invalid character in filename {filename}");
+
+            FolderPathValidation(path, filename);
         }
 
+        public static void ValidateFolderPath(string path)
+        {
+            BasePathValidation(path);
+            FolderPathValidation(path);
+        }
+
+        public static IEnumerable<string> InvalidFolderCharacters()
+        {
+            var invalidPathChars = Path.GetInvalidPathChars().Select(x => x.ToString()).ToList();
+            invalidPathChars.Add(":");
+            invalidPathChars.Add(@"\\");
+            invalidPathChars.Add(@"*");
+            invalidPathChars.Add(@"?");
+            return invalidPathChars;
+        }
 
         private static bool IsShare(string path)
         {
             return path.StartsWith(@"\\");
         }
 
-        private static bool ValidateDiskLabel(string label)
+        private static void BasePathValidation(string path)
         {
-            if (string.IsNullOrWhiteSpace(label)) return true;
-            return label == @"C:\";
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("path is empty", nameof(path));
+            if (IsShare(path)) throw new ArgumentException("is share path", nameof(path));
+        }
+
+        private static void FolderPathValidation(string path, string filename = "")
+        {
+            var label = GetPathRoot(path);
+            if (!IsValidDiskLabel(label)) throw new ArgumentException($"wrong disk label '{label}'");
+
+            var foldername = GetMiddlePathSegment(path, label, filename);
+            if (InvalidFolderCharacters().Any(foldername.Contains)) throw new ArgumentException($"invalid character in folder '{foldername}'");
+        }
+
+        private static bool IsValidDiskLabel(string label)
+        {
+            return string.IsNullOrWhiteSpace(label)
+                   || Environment.GetLogicalDrives().Contains(label.ToUpper(CultureInfo.CurrentCulture));
+        }
+
+        private static bool IsValidFileNameChars(string fileName)
+        {
+            return !Path.GetInvalidFileNameChars()
+                .Select(x => x.ToString())
+                .Any(fileName.Contains);
+        }
+
+        private static string GetLastPathSegment(string path)
+        {
+            return path.EndsWith(Path.DirectorySeparatorChar.ToString())
+                ? string.Empty
+                : path.Split(new[] {Path.DirectorySeparatorChar}, StringSplitOptions.None).LastOrDefault();
+        }
+
+        private static string GetMiddlePathSegment(string path, string label, string filename)
+        {
+            return path.Substring(0, path.Length - filename.Length).Substring(label.Length);
         }
 
         private static string GetPathRoot(string path)
         {
             var r = new Regex(@"^(([a-zA-Z]\:)|(\\))\\");
             var x = r.Match(path);
-            if (!x.Success) return null;
+            if (!x.Success) return string.Empty;
 
             var result = x.Value;
             if (result.EndsWith(":"))
-            {
                 result += "\\";
-            }
 
             return result;
         }
---
+++ UniversalParser/Tests/PathValidatorTest.cs
@@ -0,0 +1,82 @@
+﻿namespace Tests
+{
+    using System;
+    using System.IO;
+    using Base.Utilities;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class PathValidatorTest
+    {
+        [Test]
+        public void TestValidateFilePath_InvalidCharactersError()
+        {
+            var wrongFilePath = string.Join(string.Empty, Path.GetInvalidFileNameChars());
+            Assert.Catch(typeof(ArgumentException), () => PathValidator.ValidateFilePath(wrongFilePath));
+
+            var wrongDirectoryPath = string.Join(string.Empty, PathValidator.InvalidFolderCharacters());
+            Assert.Catch(typeof(ArgumentException), () => PathValidator.ValidateFilePath(wrongDirectoryPath + "\\file.ext"));
+
+            Assert.Catch(typeof(ArgumentException), () => PathValidator.ValidateFilePath(wrongDirectoryPath + "\\" + wrongFilePath));
+        }
+
+        [Test]
+        [TestCase(null, typeof(ArgumentNullException))]
+        [TestCase("", typeof(ArgumentException))]
+        [TestCase(@"\\", typeof(ArgumentException))]
+        [TestCase(@"\\file.ext", typeof(ArgumentException))]
+        [TestCase(@"X:\file.ext", typeof(ArgumentException))]
+        [TestCase(@"C:\", typeof(ArgumentException))]
+        [TestCase(@"C:\folder\", typeof(ArgumentException))]
+        public void TestValidateFilePath_Negative(string path, Type expectedException)
+        {
+            Assert.Catch(expectedException, () => PathValidator.ValidateFilePath(path));
+        }
+
+        [Test]
+        [TestCase("xxx.ext")]
+        [TestCase("xxx")]
+        [TestCase(@"xxx\sss")]
+        [TestCase(@"C:\xxx.ext")]
+        [TestCase(@"C:\folder\xxx.ext")]
+        [TestCase(@"C:\folder\xxx")]
+        [TestCase(@"C:\folder1\folder2\xxx.ext")]
+        public void TestValidateFilePath_Positive(string path)
+        {
+            Assert.DoesNotThrow(() => PathValidator.ValidateFilePath(path));
+        }
+
+        [Test]
+        public void TestValidateFolderPath_InvalidCharactersError()
+        {
+            var wrongDirectoryPath = string.Join(string.Empty, PathValidator.InvalidFolderCharacters());
+            Assert.Catch(typeof(ArgumentException), () => PathValidator.ValidateFolderPath(wrongDirectoryPath));
+        }
+
+        [Test]
+        [TestCase(null, typeof(ArgumentNullException))]
+        [TestCase("", typeof(ArgumentException))]
+        [TestCase(@"\\", typeof(ArgumentException))]
+        [TestCase(@"\\file.ext", typeof(ArgumentException))]
+        [TestCase(@"X:\file.ext", typeof(ArgumentException))]
+        public void TestValidateFolderPath_Negative(string path, Type expectedException)
+        {
+            Assert.Catch(expectedException, () => PathValidator.ValidateFolderPath(path));
+        }
+
+        [Test]
+        [TestCase("xxx.ext")]
+        [TestCase("xxx")]
+        [TestCase(@"xxx\sss")]
+        [TestCase(@"C:\")]
+        [TestCase(@"C:\xxx.ext")]
+        [TestCase(@"C:\folder\xxx.ext")]
+        [TestCase(@"C:\folder\xxx")]
+        [TestCase(@"C:\folder1\folder2\xxx.ext")]
+        [TestCase(@"C:\folder\")]
+        public void TestValidateFolderPath_Positive(string path)
+        {
+            Assert.DoesNotThrow(() => PathValidator.ValidateFolderPath(path));
+        }
+    }
+}
